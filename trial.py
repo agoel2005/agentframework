eae6de316fa02f74ca77b58abbe1e61a53cc8eb1
Advay Goel <advaygoel@gmail.com>
2024-06-22 18:25:57 -0400

    added cycling to make the code better

--- trial.py
+++ trial.py
@@ -53,7 +53,7 @@ code_body = """def maxPoints(self, points: List[List[int]]) -> int:
                     dict[f] = set()
                 dict[f].add(tuple(a))
 
-        return max(len(v) for k, v in dict.items())"""
+        return max(len(v) for k, v in dict.items()) - 1"""
 
 # Define your agents with roles and goals
 code_understander = Agent(
@@ -67,7 +67,7 @@ code_understander = Agent(
   allow_delegation=False,
   # You can pass an optional llm attribute specifying what model you wanna use.
   # llm=ChatOpenAI(model_name="gpt-3.5", temperature=0.7),
-  tools=[search_tool]
+  #tools=[search_tool]
 )
 edge_case_understander = Agent(
   role='Software Engineer',
@@ -77,7 +77,7 @@ edge_case_understander = Agent(
   logic but also making sure that your cases cover every possible code flow in the function.
   To do so, you never consult Google. SEARCHING THE INTERNET IS BANNED!!! NO GOOGLE SEARCHING!""",
   verbose=True,
-  allow_delegation=True
+  allow_delegation=False
 )
 
 input_maker = Agent(
@@ -88,6 +88,40 @@ input_maker = Agent(
   To do so, you never consult Google. SEARCHING THE INTERNET IS BANNED!!! NO GOOGLE SEARCHING!""",
   verbose=True,
   allow_delegation=True
+)
+
+oracle = Agent(
+  role='Question Answer',
+  goal='Make sure that each input for each test case actually satisfies the desired condition.',
+  backstory="""You are extremely inquisitive and love to ask questions. So, you've been hired to the crew to ensure that each input/test case being created
+  actually tests what we expect it to. Using the list of inputs just created, go through each input and test case and make sure that the input actually actually
+  matches what the test case's description is.""",
+  verbose=True,
+  allow_delegation=True
+)
+
+code_runner  = Agent(
+  role='Code Runner',
+  goal='Given all of the test cases with inputs and outputs, make sure that our original code function produces the right answer for each input.',
+  backstory="""You love running code. You have to run the code against each test case.""",
+  verbose=True,
+  allow_delegation=False
+)
+
+output_maker = Agent(
+    role='Problem Solver',
+    goal='Given a list of test cases with their inputs as well as an understanding of what the function is supposed to do, give the output for each input.',
+    backstory="""You love solving problems.""",
+    verbose=True,
+    allow_delegation=True
+)
+
+code_fixer = Agent(
+    role='Code Writer',
+    goal='If the code failed a test case, fix the code.',
+    backstory="""You are the world's most advanced coder and can solve any coding problem.""",
+    verbose=True,
+    allow_delegation=True
 )
 
 manager = Agent(
@@ -107,22 +141,56 @@ task1 = Task(
 )
 
 task2 = Task(
-  description="""Ask your coworker for its understanding of the function. Based on the summary and the description of how each path is reached, 
+  description="""Based on the summary of the function and the description of how each path is reached, 
   think of possible edge cases that might not seem obvious at first but could possibly be handled incorrectly by the code.""",
   expected_output="List of edge cases and why you think the code might incorrectly handle them.",
   agent=edge_case_understander
 )
 
 task3 = Task(
-  description="""Ask your coworkers for the function summary, code flow, and edge case list. Brainstorm a list of possible test cases to test the function but for each test case, only provide the input (not the output as well)""",
+  description="""Based on the function summary, code flow, and edge case list, brainstorm a list of possible test cases to test the function but for each test case, only provide the input (not the output as well)""",
   expected_output="List of inputs for each test case as well as a brief description of what the test case tests.",
-  agent=edge_case_understander
+  agent=input_maker
 )
 
+task4 = Task(
+  description="""You are extremely inquisitive and love to ask questions. So, you've been hired to the crew to ensure that each input/test case being created
+  actually tests what we expect it to. Using the list of inputs just created, go through each input and test case and make sure that the input actually actually
+  matches what the test case's description is.""",
+  expected_output="List of inputs for each test case as well as a brief description of what the test case tests.",
+  agent=oracle
+)
+
+
+task5 = Task(
+  description=f"""Given a list of test cases with their inputs as well as an understanding of what the function is supposed to do, give the output for each input. Do this without running the code itself. 
+  Instead, use problem solving abilities to solve the problem for yourself to get the output. As a reminder, the original function is given here: {code_body}""",
+  expected_output="List of test cases with input, output, and what is being tested with that test case.",
+  agent=output_maker
+)
+
+task6 = Task(
+  description=f"""Given all of the test cases with inputs and outputs, chceck if our original code function produces the right answer for each input. To do so, run the original code in a IDE and see what answer is given
+  for each input. It is ok if the code output is incorrect. As a reminder, here's the code: {code_body}. 
+  
+  There will be times when you are summoned by the code writer. When that happens, you will be told to rerun new code against the test cases. Use the test cases you created previously and run the new code that is being
+  provided to you. Then, return back the list of test cases and whether they worked or did not work. Additionally, return back to the code writer the code that it just sent you so that it is able to remember it.""",
+  expected_output="List of test cases that worked and test cases that did not work",
+  agent=code_runner
+)
+task7 = Task(
+  description=f"""You are given the list of test cases and whether they succeeded or failed. For every failed test case, see why it failed and adjust the code accordingly (no delegation allowed here). After you diagnose the error,
+  fix the code so that the error is no longer there. Then, go to the code runner and have them rerun the code to ensure that all the test cases now work. Keep repeating this cycle until all of the test cases work or until 20 repetitions have occurred. 
+  Every time you prompt the code runner, give it the code as part of your query so it remembers what code to run. 
+  
+  Once all test cases come become correct, you're done.""",
+  expected_output="List of test cases as well as the new code. Additionally, make a file",
+  agent=code_fixer
+)
 # Instantiate your crew with a sequential process
 crew = Crew(
-  agents=[code_understander, edge_case_understander, input_maker],
-  tasks=[task1, task2, task3],
+  agents=[code_understander, edge_case_understander, input_maker, oracle, code_runner, output_maker, code_fixer],
+  tasks=[task1, task2, task3, task4, task5, task6, task7],
   verbose=2, # You can set it to 1 or 2 to different logging levels
   cache = True,
   memory=False,
